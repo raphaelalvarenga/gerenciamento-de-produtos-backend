732b7c14ddc943646da28efeadd7eadf2349858a
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-07-02 20:27:38 -0300

    [App class] Created the App class, that will work as the backend application server. Set the express, bodyParser to get JSON structures, cors to accept external requests and set a simple '/' route for GET requests. A test has been made with the server running and it's been successful.

---
+++ src/classes/App.ts
@@ -0,0 +1,38 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+// An instance of this class represents the server
+export default class App {
+
+    // This variable will do Express' job
+    private server: Application;
+
+    // When the server starts, it needs to set the port, cors, routes, etc...
+    constructor(private port?: number | string) {
+        this.server = express();
+        this.settings();
+        this.routes();
+    }
+
+    // This settings is called to set things the server will need to work
+    public settings() {
+        this.server.use(bodyParser.urlencoded({ extended: false }));
+        this.server.use(bodyParser.json());
+        this.server.use(cors());
+        this.server.set("port", process.env.PORT || this.port || 3000);
+    }
+
+    // Configuring routes
+    public routes() {
+        this.server.get("/", (req: Request, res: Response) => {
+            console.log("Route '/' called");
+            res.json({ok: true});
+        })
+    }
+
+    // Starting server when called
+    public listen() {
+        this.server.listen(this.server.get("port"), () => console.log(`Port ${this.server.get("port")} running...`));
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("First test");
+import App from "./classes/App";
+
+// This is an instance of a server. Feel free to change the port as a parameter if you please
+const app = new App();
+
+// This will launch the app
+app.listen();
