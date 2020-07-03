74be3a40ff39b0472129cac4b6ed85ddc4b79acf
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-07-02 21:59:08 -0300

    [Login] Created hard-coded login for login. Since there is no database connection at this point yet, a login data has been created to simulate what would return in a real SQL and they have used to validate a login. The md5 dependecy has been added to the app so a token can be created using a rule that concatenates the idLogin with a timestamp generated at the exact moment the login is made. A dev dependency called @types/md5 has been added to so it can be used with TypeScript.

--- src/controllers/login-controller.ts
+++ src/controllers/login-controller.ts
@@ -1,8 +1,39 @@
 import { Request, Response} from "express";
+import RequestInterface from "../interfaces/request-interface";
+import ResponseInterface from "../interfaces/response-interface";
+import UsuarioInterface from "../interfaces/usuario-interface";
+import md5 from "md5";
 
 const loginController = (req: Request, res: Response) => {
-    console.log(req.body);
-    res.json({route: "login"});
+
+    const request: RequestInterface = req.body;
+
+    // In this point, backend will connect to the database
+
+    // The code below is a hard code just to simulate what database returns while a connection isn't being used yet
+    
+    const sql: UsuarioInterface = {
+        idLogin: 1,
+        nome: "Raphael Alvarenga do Carmo",
+        email: "raphaelalvarenga2@gmail.com",
+        senha: "aa1bf4646de67fd9086cf6c79007026c"
+    }
+
+    const { idLogin, nome, email, senha } = sql;
+
+    // Generating a token as the following: idLogin + timestamp + hashed into MD5
+    const now = new Date().getTime();
+    const token: string = md5(`${sql.idLogin.toString()}${now.toString()}`);
+
+    // Register a log here
+
+    const response: ResponseInterface = {
+        success: true,
+        message: "",
+        params: { idLogin, token }
+    };
+
+    res.json(response);
 }
 
 export default loginController;
---
+++ src/interfaces/usuario-interface.ts
@@ -0,0 +1,6 @@
+export default interface UsuarioInterface {
+    idLogin: number;
+    nome: string;
+    email: string;
+    senha?: string;
+}
